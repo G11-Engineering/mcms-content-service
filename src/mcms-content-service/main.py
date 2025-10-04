83875e56d4bc1c16a9c020e0abc1e5f39c661300
jasonmishi <jasonmishi@gmail.com>
2025-10-04 07:02:48 +0000

    add /posts GET and POST routes

--- src/mcms-content-service/main.py
+++ src/mcms-content-service/main.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI
-from .db import init_db
-from .models import Post
+from fastapi import FastAPI, Depends
+from sqlmodel import select
+from sqlmodel import Session
+from .db import get_session, init_db
+from .models import Post, PostCreate
 
 app = FastAPI()
 
@@ -12,3 +14,21 @@ def on_startup():
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/posts", response_model=list[Post])
+def get_posts(session: Session = Depends(get_session)):
+    result = session.execute(select(Post))
+    posts = result.scalars().all()
+
+    return [Post(name=post.name, content=post.content, id=post.id) for post in posts]
+
+
+@app.post("/posts")
+def add_post(post: PostCreate, session: Session = Depends(get_session)):
+    post = Post(name=post.name, content=post.content)
+    session.add(post)
+    session.commit()
+    session.refresh(post)
+
+    return post
+
