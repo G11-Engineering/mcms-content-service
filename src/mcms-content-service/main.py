6cac8ef976761abdb91250eaae885d139428ecac
jasonmishi <jasonmishi@gmail.com>
2025-10-04 06:47:07 +0000

    add sqlmodel ORM

--- src/mcms-content-service/main.py
+++ src/mcms-content-service/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+from .db import init_db
+from .models import Post
 
 app = FastAPI()
 
+
+@app.on_event("startup")
+def on_startup():
+    init_db()
 
 @app.get("/")
 async def root():
